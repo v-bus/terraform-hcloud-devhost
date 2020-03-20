22f72ad0e8ddd289084aec23ea64b8ac9f76e92b
Viktor Bushmin <viktor.bushmin@gmail.com>
2020-03-20 18:21:03 +0300

    refactor hcloud main and output :smile: modified:   nested/hcloud/main.tf modified:   nested/hcloud/outputs.tf

--- nested/hcloud/main.tf
+++ nested/hcloud/main.tf
@@ -8,7 +8,7 @@ locals {
   devs = flatten([                                                                                                               # template begining          
             for k in range(length(var.vps_list)) :                                                                            # OS's names loop
                     [
-                                                                                                                                # generate host pairs { "hostname" = "dev61-viktor-bushmin", "os" = "ubuntu" } 
+                                                                                                                                # generate host pairs { "hostname" = "dev61-username", "os" = "ubuntu" } 
                     for i in range(values(var.vps_list)[k]) : jsondecode(<<EOT
                                                                                 {
                                                                                     "hostname" : "dev${k}${i}-${var.your_login}",
@@ -20,33 +20,22 @@ locals {
                 ])
 }
 data "hcloud_image" "dev_image" {
-  count       = length(var.vps_counts)
-  name        = var.server_image[keys(var.vps_counts)[count.index]]
+  count       = length(var.vps_list)
+  name        = var.server_image[keys(var.vps_list)[count.index]]
   most_recent = true
 }
 
-resource "hcloud_ssh_key" "vb_ssh_key" {
-  name        = "vb_ssh"
-  public_key  = file(var.vb_ssh_pub_path)
+resource "hcloud_ssh_key" "dev_ssh_key" {
+  name        = "dev_ssh"
+  public_key  = file(var.dev_ssh_pub_path)
 }
 
-resource "hcloud_server" "ops-09" {
+resource "hcloud_server" "dev_vps" {
   count       = length(local.devs)
   name        = local.devs[count.index]["hostname"]
   image       = var.server_image[local.devs[count.index]["os"]]
   server_type = var.server_type
   ssh_keys    = [
-                hcloud_ssh_key.vb_ssh_key.id	
+                hcloud_ssh_key.dev_ssh_key.id	
                 ] 
-    provisioner "remote-exec" {
-         inline       = [
-                        "echo 'root:${random_password.password[count.index].result}' | chpasswd"
-                      ]
-        connection {
-          type        = "ssh"
-          user        = "root"
-          host        = self.ipv4_address
-          private_key = file(var.vb_ssh_pvt_path)
-        }
-    }               
 }
--- nested/hcloud/outputs.tf
+++ nested/hcloud/outputs.tf
@@ -1,15 +1,4 @@
 output "dev_vps" {
   value = {
-            for hz in hcloud_server.ops-09 :                              
-                hz.name => {                                                      # hcloud vps-name and hostname
-                            "ipv4 address" = hz.ipv4_address                      # vps ipv4 address
-                            "fqdn"         = element(aws_route53_record.vb_record, 
-                                                    index(hcloud_server.ops-09, hz)
-                                                    ).fqdn                        # vps fqdn
-                            "vps status"   = hz.status                            # vps status
-                            "root password"= element(random_password.password,
-                                                   index(hcloud_server.ops-09, hz)
-                                                    ).result                      # generated password
-                          }       
-          }
+            hcloud_server.dev_vps.*.name =  hcloud_server.dev_vps.*.ipv4_address                     	} 
 }
