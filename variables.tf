f5ef33034cb4777159019300acc85b8ce3132167
Viktor Bushmin <viktor.bushmin@gmail.com>
2020-03-22 09:37:45 +0300

    Update description

--- variables.tf
+++ variables.tf
@@ -9,6 +9,7 @@ variable "dns_dev_zone" {
 }
 variable "vps_list" { 
   description = "list of VPSs names should be the same as var.server_image. example {\"ubuntu_18\" = 2}"
+  type = map(any)
 } 
 variable "dev_user" {
   description = "postfix in hostname like dev$N-username"
