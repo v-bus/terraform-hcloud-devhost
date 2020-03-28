350e4c96167e79752c6b0aebf9609a83955cf407
Viktor Bushmin <38643520+v-bus@users.noreply.github.com>
2020-03-28 22:01:24 +0300

    Update outputs.tf

--- modules/aws_route53/outputs.tf
+++ modules/aws_route53/outputs.tf
@@ -1,5 +1,5 @@
 output "dns_dev_records" {
-		description = "maps of FQDN => ipv4_adress of dev host" 
+		description = "map of {FQDN = ipv4_address} of developer's host(-s)" 
 		value = {
 					for dns_record in aws_route53_record.aws_dev_record : dns_record.fqdn => tolist(dns_record.records)[0]
 				} 
